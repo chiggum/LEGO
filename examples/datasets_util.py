fix diam normalization, update notebooks

examples/datasets_util.py
@@ -111,18 +111,19 @@ def wave_on_curvedtorus3d(
         X_noisy = X
     Xt_unit = Xt / np.linalg.norm(Xt, axis=1)[:, None]
     Xp_unit = Xp / np.linalg.norm(Xp, axis=1)[:, None]
+    tang_basis = np.concatenate([Xt_unit[:,None,:], Xp_unit[:,None,:]], axis=1)
 
-    return X_noisy, X, np.c_[np.mod(th_v, 2*np.pi), ph_v], None, (Xt_unit, Xp_unit, norm_unit)
+    return X_noisy, X, np.c_[np.mod(th_v, 2*np.pi), ph_v], None, (tang_basis, norm_unit)
 
 # generates a clean torus
 def curvedtorus3d_with_normal_dir(n=10000, density='uniform', seed=42):
-    _, X, labelsMat, _, (_, _, normal_dir) = wave_on_curvedtorus3d(
+    _, X, labelsMat, _, (tang_basis, normal_dir) = wave_on_curvedtorus3d(
         n=n, noise=0, noise_type='ortho-uniform',
         seed=seed, density=density,
         wave_amp_r=0, wave_freq_r=0, wave_amp_R=0,
         wave_freq_R=0, rmax=None
     )
-    return X, labelsMat, normal_dir
+    return X, labelsMat, tang_basis, normal_dir
 
 def read_img(fpath, grayscale=False, bbox=None):
     if grayscale:

src/lego/lego.py
@@ -58,11 +58,11 @@ def lego(
     # --- 1. Nearest Neighbors ---
     t0 = time.perf_counter() if opts['print_time'] else None
     
+    X = util.normalize_diameter(X)
     neigh_ind, neigh_dist = util.nearest_neighbors(X, opts['k_nn'], opts['metric'])
-    diam = util.point_cloud_diameter(X)
     
     if opts['print_time']:
-        print(f"[LEGO] Nearest neighbors & diameter: {time.perf_counter() - t0:.4f} s")
+        print(f"[LEGO] Diameter normalization & Nearest neighbors: {time.perf_counter() - t0:.4f} s")
 
     # --- 2. Graph Laplacian Spectrum ---
     t0 = time.perf_counter() if opts['print_time'] else None
@@ -104,7 +104,6 @@ def lego(
         Uk, Sk, Vk_T = svd(X_k, full_matrices=False) # X_k = U_kS_kV_k^T
         if opts['tikhonov']:
             reg_param = np.sum(Sk**(2*(1+opts['tikhonov_power'])))
-            reg_param = reg_param/(diam**(2*opts['tikhonov_power']))
             Sk_pinv = Sk/(Sk**2 + reg_param)
             n_survived_dim_in_pinv[k] = ambient_dim
         else:

src/lego/util.py
@@ -24,32 +24,13 @@ def nearest_neighbors(data, k_nn, metric, n_jobs=-1, sort_results=True):
         neigh_ind = np.arange(n).reshape((n,1)).astype('int')
     return neigh_ind, neigh_dist
 
-def point_cloud_diameter(points, iterations=20):
-    points = np.asarray(points)
-    if len(points) < 2:
-        return 0.0
-
-    # Start with a random point
-    current_point = points[0]
-    max_diameter = 0.0
-
-    for _ in range(iterations):
-        # Calculate squared distances from the current point to all other points
-        # (Calculating squared distances avoids the costly square root operation)
-        diffs = points - current_point
-        sq_dists = np.sum(diffs**2, axis=1) 
-        
-        # Find the index of the furthest point
-        furthest_idx = np.argmax(sq_dists)
-        
-        current_max_dist = np.sqrt(sq_dists[furthest_idx])
-        if current_max_dist > max_diameter:
-            max_diameter = current_max_dist
-            
-        # Jump to the furthest point for the next iteration
-        current_point = points[furthest_idx]
-
-    return max_diameter
+def normalize_diameter(X):
+    X = X.copy()
+    mu = X.mean(axis=0)
+    X = X - mu[None,:]
+    max_norm = np.max(np.linalg.norm(X, axis=1))
+    X /= (2*max_norm*(1 + 1e-6))
+    return X
 
 def compute_principal_angles(tang_basis_1, tang_basis_2):
     n_samples, emb_dim, _ = tang_basis_1.shape